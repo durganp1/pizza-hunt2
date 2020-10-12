293d597aa3691d2e80bd738db056aaf891036535
durganp1 <durganp1@gmail.com>
2020-10-12 11:27:28 -0500

    created pizza controlllers

---
+++ models/Pizza.js
@@ -0,0 +1,27 @@
+
+
+const { Schema, model } = require('mongoose');
+
+const PizzaSchema = new Schema({
+    pizzaName: {
+        type: String
+    },
+    createdBy: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    size: {
+        type: String,
+        default: 'Large'
+    },
+    toppings: []
+});
+
+// CREATE THE PIZZA MODEL USING THE PIZZASCHEMA
+const Pizza = model('Pizza', PizzaSchema);
+
+// EXPORT THE PIZZA MODEL
+module.exports = Pizza;
