c7a11e81c453bca3ccd6de8d73b584acbaa6b76b
durganp1 <durganp1@gmail.com>
2020-10-12 23:40:07 -0500

    adding comment routes and models

--- models/Pizza.js
+++ models/Pizza.js
@@ -3,22 +3,41 @@
 const { Schema, model } = require('mongoose');
 
 const PizzaSchema = new Schema({
-    pizzaName: {
-        type: String
+        pizzaName: {
+            type: String
+        },
+        createdBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        size: {
+            type: String,
+            default: 'Large'
+        },
+        toppings: [],
+        comments: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Comment'
+            }
+        ]
     },
-    createdBy: {
-        type: String
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    size: {
-        type: String,
-        default: 'Large'
-    },
-    toppings: []
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
+);
+
+// GET TOTAL COUNT OF COMMENTS AND REPLIES ON RETRIEVAL
+PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.length;
 });
+
 
 // CREATE THE PIZZA MODEL USING THE PIZZASCHEMA
 const Pizza = model('Pizza', PizzaSchema);
